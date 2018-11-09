41b9606ff0d89668f43ac01518f3a50286ab1eb5
Sefsef <sef.arkanoid@gmail.com>
2018-11-09 22:54:11 +0100

    Styling, dark theme, grid layouts, vertical scrolling disabled (true SPA)

--- DmApp/src/app/auth.service.ts
+++ DmApp/src/app/auth.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +10,7 @@ export class AuthService {
   loginURL: string = "http://localhost:4000/users/authenticate"; 
   registerURL: string = "http://localhost:4000/users/register";
 
-  constructor(private http: HttpClient, private router: Router) { }
+  constructor(private http: HttpClient) { }
 
   login(loginUserData) {
     return this.http.post(this.loginURL, loginUserData);
@@ -23,7 +22,6 @@ export class AuthService {
 
   logout() {
     localStorage.removeItem('token');
-    this.router.navigate(['/login']);
   }
 
   loggedIn() {
