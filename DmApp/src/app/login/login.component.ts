08627fd2996cf6439843539dc3a9991c9e9b592b
Sefsef <sef.arkanoid@gmail.com>
2018-11-09 12:15:42 +0100

    Routing after login (use navigateByUrl instead of navigate

--- DmApp/src/app/login/login.component.ts
+++ DmApp/src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -11,7 +12,7 @@ export class LoginComponent implements OnInit {
 
   loginUserData = {};
 
-  constructor(private auth: AuthService) { }
+  constructor(private auth: AuthService, private router: Router) { }
 
   ngOnInit() {
   }
@@ -22,6 +23,7 @@ export class LoginComponent implements OnInit {
       {
         console.log(response);
         localStorage.setItem('token', (response as any).token);
+        this.router.navigateByUrl('/home');
       },
       error => console.log(error)
     )
